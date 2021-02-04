ab6f9a302d900a72759d11a9eeed625ab98118b5
Adam Kozlowski <adamkozlowski0@gmail.com>
2021-02-04 14:37:40 +0000

    Exposing hit counter table

--- lib/cdkworkshop-stack.ts
+++ lib/cdkworkshop-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkworkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -18,9 +19,14 @@ export class CdkworkshopStack extends cdk.Stack {
       downstream: hello
     })
 
-    //defines an API Gateway API resource
+    //defines an API Gateway API resource backed by "hello" function
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
     });
   }
 }
